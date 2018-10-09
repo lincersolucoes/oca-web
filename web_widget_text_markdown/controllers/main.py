cbdee79bd979776d7909ac760ede76daa545b9ff
dontk1 <komolafetosin@gmail.com>
2018-10-09 20:15:17 +0100

    added model and id

--- web_widget_text_markdown/controllers/main.py
+++ web_widget_text_markdown/controllers/main.py
@@ -42,8 +42,6 @@ class Binary(http.Controller):
     @serialize_exception
     def upload_dropzone_attachment(self, model, id, ufile):
         Model = request.env['ir.attachment']
-        print('We are here now!!!!!!!!')
-        print(request.env.context)
         try:
             attachment = Model.create({
                 'name': ufile.filename,
