df31fd765fdf64aebcd62970ef9108adcccb28b2
Grover Menacho <hmen1989@gmail.com>
2018-11-27 23:20:49 -0400

    web_widget_many2one_binary: [ADD] Added support for bigger files... To be tested further

--- web_widget_many2one_binary/controllers/main.py
+++ web_widget_many2one_binary/controllers/main.py
@@ -9,6 +9,10 @@ import logging
 import werkzeug.utils
 import werkzeug.wrappers
 
+import tempfile
+import os
+
+
 from odoo import http
 from odoo.addons.website.models.website import unslug
 from odoo.addons.web.controllers.main import Binary
@@ -36,6 +40,35 @@ def serialize_exception(f):
             return werkzeug.exceptions.InternalServerError(json.dumps(error))
     return wrap
 
+
+def get_base64_from_file(ufile, chunk_size=3*3200):
+
+    chunk_size -= chunk_size % 3  # align to multiples of 3
+    try:
+        fd, path = tempfile.mkstemp()
+        try:
+            with os.fdopen(fd, 'w') as tmp:
+                # do stuff with temp file
+                while True:
+                    bin_data = ufile.read(chunk_size)
+                    if not bin_data:
+                        break
+                    b64_data = base64.b64encode(bin_data)
+                    tmp.write(b64_data)
+        except Exception, e:
+            pass
+        finally:
+            open_file = open(path, 'r')
+            b64_file = open_file.read()
+            os.remove(path)
+            return b64_file
+    except Exception, e:
+        return ''
+
+    return ''
+
+
+
 class Binary(http.Controller):
 
     @http.route('/web/binary/upload_field_attachment', type='http', auth="user")
@@ -47,9 +80,10 @@ class Binary(http.Controller):
                         win.jQuery(win).trigger(%s, %s);
                     </script>"""
         try:
+            b64_file = get_base64_from_file(ufile)
             attachment = Model.create({
                 'name': ufile.filename,
-                'datas': base64.encodestring(ufile.read()),
+                'datas': b64_file,
                 'datas_fname': ufile.filename,
                 'res_model': model,
                 'res_id': id and int(id) or 0,
